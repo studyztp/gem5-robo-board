bfa6e5d445471f68f62823371b9027d002854b90
studyztp <studyztp@gmail.com>
2025-09-08 09:36:52 -0700

    add FPU FU for M4 core

--- demo-board/cores/m4_core.py
+++ demo-board/cores/m4_core.py
@@ -1,10 +1,6 @@
 from typing import Optional
-
 from m5.objects import (
-    BaseCPU,
-    BaseMMU,
-    Port,
-    Process,
+    OpClass
 )
 from m5.objects.ArmCPU import ArmMinorCPU
 from m5.objects.BaseMinorCPU import *
@@ -15,6 +11,78 @@ from gem5.components.processors.cpu_types import CPUTypes
 from gem5.isas import ISA
 from gem5.utils.override import overrides
 from gem5.utils.requires import requires
+
+def FPMaker(
+    _opClasses: list[str], 
+    _opLat: int, 
+    _timingDescription:str, 
+    _srcRegsRelativeLats: 0
+)-> MinorFU:
+    class CustomFU(MinorFU):
+        opClasses = minorMakeOpClassSet(_opClasses)
+        opLat = _opLat
+        timings = [MinorFUTiming(
+            description=_timingDescription, 
+            srcRegsRelativeLats=_srcRegsRelativeLats
+            )
+        ]
+    return CustomFU()
+
+def CortexM4FPUPool() -> list[MinorFU]:
+    # Floating point unit
+    # TODO: understand the exact opClasses supported by M4 FPU. Currently, we 
+    # allow the following opClasses which cover most of the floating point
+    # operations.
+
+    # Number of cycle is found in Cortex-M4 Technical Reference Manual
+    # Table 7-1  FPU instruction set.
+    # Combine with the Arm ISA in gem5/src/arch/arm/isa/insts/fp.isa, Cortex M4
+    # supports the following floating point operations:
+    # vabs.f32, vmov, vmrs, vneg
+    floatAbs = FPMaker(["SimdFloatMisc"], 1, "SimdFloatMisc", 2)
+    # vadd.f32, vsub
+    floatAdd = FPMaker(["SimdFloatAdd"], 1, "SimdFloatAdd", 2)
+    # vcmp.f32 and vcmpe.f32
+    floatCmp = FPMaker(["SimdFloatCmp"], 1, "SimdFloatCmp", 2)
+    # vcvt.f32
+    floatCvt = FPMaker(["SimdFloatCvt"], 1, "SimdFloatCvt", 2)
+    # vdiv.f32
+    floatDiv = FPMaker(["SimdFloatDiv"], 14, "SimdFloatDiv", 2)
+    # vmul
+    floatMul = FPMaker(["SimdFloatMult"], 1, "SimdFloatMult", 2)
+    # vmla, vmls, vnmla, vnmls, vfma, vfms, vfnma, vfnms
+    floatMla = FPMaker(["SimdFloatMultAcc"], 3, "SimdFloatMultAcc", 2)
+    # vsqrt
+    floatSqrt = FPMaker(["SimdFloatSqrt"], 14, "SimdFloatSqrt", 2)
+    # vldr.32
+    floatMemRead = FPMaker(
+        ["FloatMemRead"], 2, "FloatMemRead", 2
+    )
+    # vstr.32
+    floatMemWrite = FPMaker(
+        ["FloatMemWrite"], 2, "FloatMemWrite", 2
+    )
+
+    # TODO: need to dig more precisely with vldm, vpop, vpush, vstm
+
+    return [
+        floatAbs,
+        floatAdd,
+        floatCmp,
+        floatCvt,
+        floatDiv,
+        floatMul,
+        floatMla,
+        floatSqrt,
+        floatMemRead,
+        floatMemWrite,
+    ]
+
+def CortexM4IntFU() -> list[MinorFU]:
+    # Integer instruction units
+    # Number of cycle is found in Cortex-M4 Technical Reference Manual
+    # Table 3-1  Cortex-M4 instruction set. 
+    return []
 
 
 class CortexM4Core(ArmMinorCPU):
@@ -35,6 +103,17 @@ class CortexM4Core(ArmMinorCPU):
         # Width (in instructions) of input to Decode (and implicitly Decode's
         # own width)
         self.decodeInputWidth = 1
+
+        self.executeFuncUnits = self._create_fu_pool()
+
+    def _create_fu_pool(self) -> list[MinorFU]:
+        _all_fus = []
+        if self._if_fpu:
+            _all_fus += CortexM4FPUPool()
+        _all_fus += CortexM4IntFU()
+        class CortexM4FUPool(MinorFUPool):
+            funcUnits = _all_fus
+        return CortexM4FUPool()
 
 
 class CortexM4CPU(BaseCPUCore):
